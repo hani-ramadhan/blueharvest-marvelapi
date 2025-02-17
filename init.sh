dda2737d472cc32dce21d395dc3641cbe1acced3
hani-ramadhan <haniramadhan@hotmail.com>
2025-02-17 02:06:43 +0100

    edited init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
-
-# Create required directories
-mkdir -p /opt/airflow/data/marvel/{raw,logs,processed,analytics}/{latest,$(date +%Y%m%d)}
-
-# Set ownership to airflow user
-chown -R airflow:root /opt/airflow/data
-
-# Set directory permissions
-chmod -R 775 /opt/airflow/data
-
-echo "Initialized data directories with correct permissions"
-
+# Create base project directories
+mkdir -p data/marvel/{raw,logs,processed,analytics}/{latest,$(date +%Y%m%d)}
 mkdir -p logs
+
+# Set permissions
+chmod -R 777 data
 chmod -R 777 logs
+
+echo "Initialized directories with correct permissions"
